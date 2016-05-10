851f946d86c928ef6392d68bd23449a3a57a00c4
x620 <romanchenko@it-projects.info>
2016-05-10 13:23:10 +0500

    [DOC] adds icons and images for app store
    [DOC]  mail-sent pics
    
    [FIX] pics

--- mail_sent/__openerp__.py
+++ mail_sent/__openerp__.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name' : 'Sentbox',
-    'version' : '1.0.2',
-    'author' : 'IT-Projects LLC, Ivan Yelizariev',
-    'license': 'GPL-3',
-    'category' : 'Discuss',
-    'website' : 'https://twitter.com/yelizariev',
+    "name": "Sentbox",
+    "summary": """Quick way to find sent messages""",
+    "category": "Discuss",
+    "images": ['images/menu.png'],
+    "version": "1.0.3",
+    "author": "IT-Projects LLC, Ivan Yelizariev, Pavel Romanchenko",
+    "website": "https://it-projects.info",
+    "license": "LGPL-3",
     'price': 40.00,
     'currency': 'EUR',
-    'depends' : ['mail'],
-    'data':[
+    'depends': ['mail'],
+    'data': [
         'views.xml',
-        ],
+    ],
     'installable': True
 }
