a2ec257f9f549af9e339b5dbd4b717fcbed7000d
x620 <romanchenko@it-projects.info>
2016-05-25 17:30:39 +0500

    [DOC] Init commit: docs, manifests and description
    [DOC] corrected name of menu item
    
    [DOC] corrected screenshots and description
    
    [DOC] updated screenshot, corrected description in index.html and create images folder

--- mail_all/README.rst
+++ mail_all/README.rst
@@ -2,15 +2,15 @@
  Show all messages
 ===================
 
-Adds ``Discuss / All`` menu, that shows all messages accesable by current user
+Adds ``Messaging / All messages`` menu, that shows all messages accesable by current user
 
 Further information
 -------------------
 
-HTML Description: https://apps.odoo.com/apps/modules/9.0/mail_all/
+HTML Description: https://apps.odoo.com/apps/modules/8.0/mail_all/
 
 Usage instructions: `<doc/index.rst>`_
 
 Changelog: `<doc/changelog.rst>`_
 
-Tested on Odoo 9.0 d3dd4161ad0598ebaa659fbd083457c77aa9448d
+Tested on Odoo 8.0 0af32f3f84bae07b11abb8538d02e35c7369a348
--- mail_all/__openerp__.py
+++ mail_all/__openerp__.py
@@ -2,27 +2,27 @@
 {
     "name": "Show all messages",
     "summary": """Checkout all messages where you have access""",
-    "category": "Discuss",
-    "images": ['images/1.jpg'],
+    "category": "Social Network",
+    "images": ['images/1.png'],
     "version": "1.0.0",
 
     "author": "IT-Projects LLC, Pavel Romanchenko",
     "website": "https://it-projects.info",
-    "license": "LGPL-3",
-    'price': 40.00,
-    'currency': 'EUR',
+    "license": "GPL-3",
+    "price": 40.00,
+    "currency": "EUR",
 
     "depends": [
-        "mail_base"
+        "mail",
     ],
     "external_dependencies": {"python": [], "bin": []},
     "data": [
         "views/templates.xml",
     ],
-    "qweb": [
-        "static/src/xml/menu.xml",
-    ],
     "demo": [],
-    'installable': True,
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "installable": True,
     "auto_install": False,
 }
