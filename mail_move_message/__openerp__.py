60f56006e3ffc78bef2b344bbc53d1acd04415c0
x620 <romanchenko@it-projects.info>
2016-05-30 11:34:56 +0500

    [PORT] Init commit: change category and installable parameters in __openerp__.py
    [PORT] Parameter of message "to_read" replaced to "needaction" in mail_move_message_models.py
    [PORT] Updated mail_move_message.js for 9.0 compatible

--- mail_move_message/__openerp__.py
+++ mail_move_message/__openerp__.py
@@ -3,11 +3,11 @@
     'version' : '1.0.4',
     'author' : 'IT-Projects LLC, Ivan Yelizariev',
     'license': 'LGPL-3',
-    'category' : 'Social Network',
+    'category' : 'Discuss',
     'website' : 'https://twitter.com/yelizariev',
     'price': 9.00,
     'currency': 'EUR',
-    'depends' : ['mail', 'web_polymorphic_field'],
+    'depends' : ['mail_base', 'web_polymorphic_field'],
     'images': ['images/inbox.png'],
     'data':[
         'mail_move_message_views.xml',
@@ -16,5 +16,5 @@
     'qweb': [
         'static/src/xml/mail_move_message_main.xml',
     ],
-    'installable': False
+    'installable': True,
 }
