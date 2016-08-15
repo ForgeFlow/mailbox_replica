2b4f70fd9b64e52a64f86164fa8c30df08c1a623
x620 <romanchenko@it-projects.info>
2016-08-15 15:21:57 +0500

    [REF] Fix lint errors
    [REF] fix lint errors (9.0)
    
    [FIX] 8.0-travis-fix (#58)
    
    * [FIX] CamelCase
    
    * [FIX] Changing signs of the tabs to 4 spaces
    
    * [FIX] relative-import
    
    * [FIX] delete unused imports
    
    * [FIX] delete prints
    
    * [FIX] lint for js
    
    * [FIX] Duplicate implicit target name: "changelog"
    
    * [FIX] links in rst files
    
    * [FIX] flake8 E115
    
    * [FIX] pylint E1124
    
    * [FIX] pylint W1401
    
    [FIX] 9.0 travis fix (#59)
    
    * [FIX] autopep8
    
    * [FIX] CamelCase
    
    * [FIX] relative-import
    
    * [FIX] delete unused imports
    
    * [FIX] delete prints
    
    * [FIX] fix comments
    
    * [FIX] lint for js
    
    * [FIX] Add the first line (coding) in py-files
    
    * [FIX] Duplicate implicit target name: "changelog"
    
    * [FIX] links in rst files
    
    * [FIX] flake8 F841
    
    * [FIX] flake8 F401
    
    * [FIX] triple underscore

--- mail_sent/models.py
+++ mail_sent/models.py
@@ -11,22 +11,18 @@ class MailMessage(models.Model):
     @api.depends('author_id', 'partner_ids')
     def _get_sent(self):
         self_sudo = self.sudo()
-        self_sudo.sent = len(self_sudo.partner_ids) > 1 \
-                         or len(self_sudo.partner_ids) == 1 \
-                            and self_sudo.author_id \
-                            and self_sudo.partner_ids[0].id != self_sudo.author_id.id
+        self_sudo.sent = len(self_sudo.notified_partner_ids) > 1 or len(self_sudo.notified_partner_ids) == 1 and self_sudo.author_id and self_sudo.notified_partner_ids[0].id != self_sudo.author_id.id
 
-    @api.multi
-    def message_format(self):
-        message_values = super(MailMessage, self).message_format()
-        message_index = {message['id']: message for message in message_values}
-        for item in self:
-            msg = message_index.get(item.id)
-            if msg:
-                msg['sent'] = item.sent
-        return message_values
+
+class MailNotification(models.Model):
+    _inherit = 'mail.notification'
+
+    def _notify(self, cr, uid, message_id, **kwargs):
+        super(MailNotification, self)._notify(cr, uid, message_id, **kwargs)
+        self.pool['mail.message'].browse(cr, uid, message_id)._get_sent()
 
 
 class MailComposeMessage(models.TransientModel):
+
     _inherit = 'mail.compose.message'
     sent = fields.Boolean('Sent', help='dummy field to fix inherit error')
