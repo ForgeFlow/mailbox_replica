40d36cc5d121cfb29e34eb629da9dd3e1668fc3c
Ivan Yelizariev <yelizariev@it-projects.info>
2017-01-18 17:49:23 +0500

    [DOC] add support email

--- mail_sent/__openerp__.py
+++ mail_sent/__openerp__.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Sentbox',
-    'version': '1.0.3',
-    'author': 'IT-Projects LLC, Ivan Yelizariev',
-    'license': 'GPL-3',
-    'category': 'Discuss',
-    'website': 'https://twitter.com/yelizariev',
+    "name": "Sentbox",
+    "summary": """Quick way to find sent messages""",
+    "category": "Discuss",
+    "images": ['images/menu.png'],
+    "version": "1.0.3",
+
+    "author": "IT-Projects LLC, Ivan Yelizariev, Pavel Romanchenko",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info",
+    "license": "LGPL-3",
     'price': 40.00,
     'currency': 'EUR',
     'depends': ['mail'],
