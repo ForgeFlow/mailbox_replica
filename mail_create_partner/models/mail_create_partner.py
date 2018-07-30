cc619502c2aece3cc7cd390fff4e84125ea7e280
mreficent <miquel.raich@eficent.com>
2018-07-30 13:44:58 +0200

    [MIG] mail_create_partner: Migration to 10.0

--- mail_create_partner/models/mail_create_partner.py
+++ mail_create_partner/models/mail_create_partner.py
@@ -1,18 +1,20 @@
-from openerp.osv import fields, osv
-from openerp import SUPERUSER_ID
-from openerp.tools import email_split
-from openerp import models
+# -*- coding: utf-8 -*-
+# Copyright 2016-18 Eficent (<http://www.eficent.com/>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-class MailCreatePartnerWizard(osv.osv_memory):
+from odoo import api, fields, models
+from odoo.tools.mail import email_split
+
+
+class MailCreatePartnerWizard(models.TransientModel):
     _name = 'mail_create_partner.wizard'
 
-    def default_get(self, cr, uid, fields_list, context=None):
-        res = super(MailCreatePartnerWizard, self).default_get(cr, uid,
-                                                               fields_list,
-                                                               context=context)
-        if 'default_message_id' in context:
-            message = self.pool['mail.message'].browse(
-                cr, uid, context['default_message_id'], context=context)
+    @api.model
+    def default_get(self, fields_list):
+        res = super(MailCreatePartnerWizard, self).default_get(fields_list)
+        if 'default_message_id' in self._context:
+            message = self.env['mail.message'].browse(
+                self._context['default_message_id'])
             res['can_create_partner'] = not message.author_id
             res['partner_id'] = message.author_id.id
             email_from = message.email_from
@@ -28,30 +30,27 @@ class MailCreatePartnerWizard(osv.osv_memory):
                 res['message_email_from'] = email
         return res
 
-    def _get_can_create_partner(self, cr, uid, ids, name, args, context=None):
+    @api.multi
+    def _compute_can_create_partner(self):
         # message was not moved before OR message is a top message of
         # previous move
-        res = dict.fromkeys(ids, False)
-        for record in self.browse(cr, uid, ids, context=context):
+        res = {}
+        for record in self:
             res[record.id] = not record.message_id.author_id
         return res
 
-    _columns = {
-        'message_id': fields.many2one('mail.message', string='Message'),
-        'message_body': fields.related('message_id', 'body',
-                                       type='html', string='Message to move',
-                                       readonly=True),
-        'can_create_partner': fields.function(_get_can_create_partner,
-                                              method=True, type='boolean',
-                                              string='Can move'),
-        'partner_id': fields.many2one('res.partner', string='Author'),
-        'message_email_from': fields.char('Email From'),
-        'message_name_from': fields.char('Name From'),
-    }
+    message_id = fields.Many2one(comodel_name='mail.message',
+                                 string='Message')
+    message_body = fields.Html(related='message_id.body',
+                               string='Message to move', readonly=True)
+    can_create_partner = fields.Boolean(compute='_compute_can_create_partner',
+                                        string='Can move')
+    partner_id = fields.Many2one(comodel_name='res.partner', string='Author')
+    message_email_from = fields.Char('Email From')
+    message_name_from = fields.Char('Name From')
 
-    def create_partner(self, cr, uid, message_id, context=None):
-        message = self.pool['mail.message'].browse(cr, uid, message_id,
-                                                   context=context)
+    def create_partner(self, message_id):
+        message = self.env['mail.message'].browse(message_id)
         email_from = message.email_from
         message_name_from = False
         message_email_from = False
@@ -61,43 +60,40 @@ class MailCreatePartnerWizard(osv.osv_memory):
             if parts:
                 email = parts[0]
                 name = email_from[:email_from.index(email)].\
-                    replace('"', '').replace('<', '').strip() \
-                    or email_from
+                    replace('"', '').replace('<', '').strip() or email_from
             else:
                 name, email = email_from
             message_name_from = name
             message_email_from = email
         if message_name_from:
-            context.update({'update_message_author': True})
-            partner_id = self.pool['res.partner'].create(
-                cr, uid, {'name': message_name_from,
-                          'email': message_email_from}, context=context)
+            self._context.update({'update_message_author': True})
+            partner_id = self.env['res.partner'].create({
+                'name': message_name_from,
+                'email': message_email_from,
+            })
         context = {'partner_id': partner_id}
 
         return context
 
 
-class ResPartner(models.Model):
+class Partner(models.Model):
     _inherit = 'res.partner'
 
-    def create(self, cr, uid, vals, context=None):
-        if context is None:
-            context = {}
-        res_id = super(ResPartner, self).create(cr, uid, vals, context=context)
-        if 'update_message_author' in context and 'email' in vals:
-            mail_message_obj = self.pool['mail.message']
+    @api.model
+    def create(self, vals):
+        res_id = super(Partner, self).create(vals).id
+        if 'update_message_author' in self._context and 'email' in vals:
+            mail_message_obj = self.env['mail.message']
             # Escape special SQL characters in email_address to
             # avoid invalid matches
             email_address = (vals['email'].replace('\\', '\\\\').
                              replace('%', '\\%').replace('_', '\\_'))
             email_brackets = "<%s>" % email_address
-            messages = mail_message_obj.search(cr, uid, [
-                                '|',
-                                ('email_from', '=ilike', email_address),
-                                ('email_from', 'ilike', email_brackets),
-                                ('author_id', '=', False)
-                            ], context=context)
+            messages = mail_message_obj.search([
+                '|', ('email_from', '=ilike', email_address),
+                ('email_from', 'ilike', email_brackets),
+                ('author_id', '=', False),
+            ])
             if messages:
-                mail_message_obj.write(cr, SUPERUSER_ID, messages,
-                                       {'author_id': res_id}, context=context)
+                messages.write({'author_id': res_id})
         return res_id
