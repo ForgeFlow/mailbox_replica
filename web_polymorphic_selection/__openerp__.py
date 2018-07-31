9c2250c6ba3ad8bbfa7ba9722906d81eaa12210c
mreficent <miquel.raich@eficent.com>
2018-07-31 14:26:19 +0200

    [UPD] Rename web_polymorphic_field to web_polymorphic_selection

--- web_polymorphic_selection/__openerp__.py
+++ web_polymorphic_selection/__openerp__.py
@@ -17,7 +17,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #
-{'name': 'Web Polymorphic Field',
+{'name': 'Web Polymorphic Selection',
  'version': '0.2',
  'category': 'Web',
  'depends': ['web'],
