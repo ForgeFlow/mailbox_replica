aceaf3d48d181715aa9ab03cd80020438e2c087f
mreficent <miquel.raich@eficent.com>
2018-07-31 17:23:31 +0200

    [MIG] Complete migration to v10 of polymorphic modules

--- web_polymorphic_selection/README.rst
+++ web_polymorphic_selection/README.rst
@@ -1,6 +1,14 @@
-Add a new widget named "polymorphic"
+.. image:: https://img.shields.io/badge/license-GPL--3-blue.png
+   :target: https://www.gnu.org/licenses/gpl
+   :alt: License: GPL-3
+
+=========================
+Web Polymorphic Selection
+=========================
+
+This module adds a new widget named "polymorphic".
 The polymorphic field allow to dynamically store an id linked to any model in
-Odoo instead of the usual fixed one in the view definition
+Odoo instead of the usual fixed one in the view definition.
 
 E.g::
 
--- web_polymorphic_selection/__manifest__.py
+++ web_polymorphic_selection/__manifest__.py
@@ -1,33 +1,20 @@
 # -*- coding: utf-8 -*-
-#
-#
-#    Copyright (C) 2014-2015 Augustin Cisterne-Kaas (ACK Consulting Limited)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
-{'name': 'Web Polymorphic Selection',
- 'version': '0.2',
- 'category': 'Web',
- 'depends': ['web'],
- 'author': 'Augustin Cisterne-Kaas',
- 'license': 'GPL-3',
- 'data': [
-     'views/web_polymorphic_selection.xml'
- ],
- 'js': [
-     'static/src/js/view_form.js'
- ],
- 'installable': True,
- 'application': False}
+# Copyright 2014-2015 Augustin Cisterne-Kaas (ACK Consulting Limited)
+# License GPL-3.0 or later (https://www.gnu.org/licenses/gpl).
+{
+    'name': 'Web Polymorphic Selection',
+    'version': '10.0.1.0.0',
+    'category': 'Web',
+    'depends': ['web'],
+    'author': 'Augustin Cisterne-Kaas',
+    'license': 'GPL-3',
+    'summary': 'Add polymorphic widget for Selection fields',
+    'website': 'https://www.github.com/Eficent/mailbox_replica/tree/10.0',
+    'data': [
+        'views/web_polymorphic_selection.xml'
+    ],
+    'js': [
+        'static/src/js/view_form.js'
+    ],
+    'installable': True,
+}
