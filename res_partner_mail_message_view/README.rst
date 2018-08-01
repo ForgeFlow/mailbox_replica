8ed9cb83c4f96ed3c0a87fa2b7573f10f521946a
mreficent <miquel.raich@eficent.com>
2018-08-01 16:17:46 +0200

    [MIG] res_partner_mail_message_view: Migration to 10.0

---
+++ res_partner_mail_message_view/README.rst
@@ -0,0 +1,10 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+=========================
+Partner Mail Message View
+=========================
+
+This module provides a button to view the messages that a partner has
+received or sent. System notifications are excluded.
---
+++ res_partner_mail_message_view/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 Eficent (<http://www.eficent.com/>)
+#             <contact@eficent.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Mail Message View",
+    "version": "10.0.1.0.0",
+    "author": "Eficent",
+    "summary": "Mail messages in partners",
+    "website": "https://www.github.com/Eficent/mailbox_replica/tree/10.0",
+    "category": "Social Network",
+    "license": "AGPL-3",
+    "depends": ["mail"],
+    'data': [
+        'views/res_partner_view.xml'
+    ],
+    'installable': True,
+}
