5a623aef99eef8c6e4668222ab6ae1260480a787
mreficent <miquel.raich@eficent.com>
2018-08-01 15:15:10 +0200

    [MIG] fetchmail_fetch_missing: Migration to 10.0

--- fetchmail_fetch_missing/models/mail_message.py
+++ fetchmail_fetch_missing/models/mail_message.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 # Copyright 2018 Eficent Business and IT Consulting Services, S.L.
 #           <http://www.eficent.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models
+from odoo import api, models
 
 
 class Message(models.Model):
