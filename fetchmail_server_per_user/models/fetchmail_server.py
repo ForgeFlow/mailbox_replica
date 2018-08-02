2e042f0e3f0b1ba9dda603a71fbaf7502c861eab
mreficent <miquel.raich@eficent.com>
2018-08-02 14:57:21 +0200

    [MIG] fetchmail_server_per_user: Migration to 10.0

--- fetchmail_server_per_user/models/fetchmail_server.py
+++ fetchmail_server_per_user/models/fetchmail_server.py
@@ -1,25 +1,9 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2015 Eficent (<http://www.eficent.com/>)
-#              <contact@eficent.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2015 Eficent (<http://www.eficent.com/>)
+#             <contact@eficent.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 import logging
-from openerp import models, fields
+from odoo import fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -27,10 +11,11 @@ _logger = logging.getLogger(__name__)
 class FetchMailServer(models.Model):
     _inherit = 'fetchmail.server'
 
-    def _get_current_user(self, cr, uid, context=None):
-        return uid
+    def _get_current_user(self):
+        return self._uid
 
-    user_id = fields.Many2one('res.users', string='Owner', default=_get_current_user)
+    user_id = fields.Many2one(
+        comodel_name='res.users', string='Owner', default=_get_current_user)
 
     _sql_constraints = [
         ('fetchmail_user_uniq', 'unique(user_id)', 'That user already has a '
