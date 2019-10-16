1f409591b8613512ffc099ab64e9607589db17e2
ahenriquez <ahenriquez@eficent.com>
2019-10-16 15:53:51 +0200

    [MIG]smtp_per_user to v12

--- smtp_per_user/__manifest__.py
+++ smtp_per_user/__manifest__.py
@@ -1,19 +1,15 @@
-# -*- coding: utf-8 -*-
 # Author: Andrius Laukavičius. Copyright: JSC Boolit.
 # See LICENSE file for full copyright and licensing details.
 
 {
-    'name': "SMTP Per User",
-    'version': '10.0.1.0.0',
-    'summary': 'Send letters from Odoo using your own mail',
-    'category': 'Mail',
-    'author': 'Boolit',
-    'license': 'LGPL-3',
-    'website': "https://www.github.com/Eficent/mailbox_replica/tree/10.0",
-    "depends": ['mail'],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/smtp_per_user_view.xml',
-    ],
+    "name": "SMTP Per User",
+    "version": "12.0.1.0.0",
+    "summary": "Send letters from Odoo using your own mail",
+    "category": "Mail",
+    "author": "Boolit",
+    "license": "LGPL-3",
+    "website": "https://www.github.com/Eficent/mailbox_replica",
+    "depends": ["mail"],
+    "data": ["security/ir.model.access.csv", "views/smtp_per_user_view.xml"],
     "installable": True,
 }
--- smtp_per_user/models/smtp_per_user.py
+++ smtp_per_user/models/smtp_per_user.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from email.utils import parseaddr, formataddr
 from odoo import api, fields, models
 
@@ -7,12 +5,14 @@ from odoo import api, fields, models
 class IrMailServer(models.Model):
     _inherit = "ir.mail_server"
 
-    user_id = fields.Many2one('res.users', string="Owner")
-    email_name = fields.Char('Email Name', help="Overrides default email name")
-    force_use = fields.Boolean('Force Use',
-                               help="If checked and this server is chosen to "
-                                    "send mail message, It will ignore owners "
-                                    "mail server")
+    user_id = fields.Many2one("res.users", string="Owner")
+    email_name = fields.Char("Email Name", help="Overrides default email name")
+    force_use = fields.Boolean(
+        "Force Use",
+        help="If checked and this server is chosen to "
+        "send mail message, It will ignore owners "
+        "mail server",
+    )
 
     @api.model
     def replace_email_name(self, old_email):
@@ -27,22 +27,26 @@ class IrMailServer(models.Model):
 
 
 class MailMail(models.Model):
-    _inherit = 'mail.mail'
+    _inherit = "mail.mail"
 
     @api.multi
     def send(self, auto_commit=False, raise_exception=False):
-        ir_mail_server_obj = self.env['ir.mail_server']
-        res_user_obj = self.env['res.users']
+        ir_mail_server_obj = self.env["ir.mail_server"]
+        res_user_obj = self.env["res.users"]
         for email in self:
             if not email.mail_server_id.force_use:
                 user = res_user_obj.search(
-                    [('partner_id', '=', email.author_id.id)], limit=1)
+                    [("partner_id", "=", email.author_id.id)], limit=1
+                )
                 if user:
                     mail_server = ir_mail_server_obj.search(
-                        [('user_id', '=', user.id)], limit=1)
+                        [("user_id", "=", user.id)], limit=1
+                    )
                     if mail_server:
                         email.mail_server_id = mail_server.id
             email.email_from = email.mail_server_id.replace_email_name(
-                email.email_from)
-        return super(MailMail, self).send(auto_commit=auto_commit,
-                                          raise_exception=raise_exception)
+                email.email_from
+            )
+        return super(MailMail, self).send(
+            auto_commit=auto_commit, raise_exception=raise_exception
+        )
