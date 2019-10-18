c937010e74dd7b7a4ec458d5f032dc4f2fab27f7
aheficent <ahenriquez@eficent.com>
2019-10-19 00:05:36 +0200

    [MIG]partner_mail_message_view to v12

--- partner_mail_message_view/__manifest__.py
+++ partner_mail_message_view/__manifest__.py
@@ -1,19 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014 Eficent (<http://www.eficent.com/>)
 #             <contact@eficent.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Partner Mail Message View",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent",
     "summary": "Mail messages in partners",
     "website": "https://www.github.com/Eficent/mailbox_replica/tree/10.0",
     "category": "Social Network",
     "license": "AGPL-3",
     "depends": ["mail"],
-    'data': [
-        'views/res_partner_view.xml'
-    ],
-    'installable': True,
+    "data": ["views/res_partner_view.xml"],
+    "installable": True,
 }
